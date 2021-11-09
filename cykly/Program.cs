ff35866294cc64dd4e59650057eefc3c444577e5
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2021-11-09 09:33:06 +0100

    Přidejte soubory projektu.

---
+++ cykly/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cykly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 varianta
+            int cislo = 1;
+
+            while(cislo < 100)
+            {
+                Console.WriteLine(cislo);
+                cislo++;
+            }
+            Console.ReadLine();
+
+            // 2 varianta
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.ReadLine();
+            
+
+        }  
+    }
+}
